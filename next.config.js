04f7db35470b52c56c20c2ccf3416d5cf313d063
Christian <MrBanditHat@gmail.com>
2023-06-13 13:37:09 -0700

    image imports

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['oaidalleapiprodscus.blob.core.windows.net'],
+  },
+  // other configuration options
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@
 import styles from '@/styles/Home.module.scss'
 import {useState, useRef} from 'react'
 import axios from 'axios'
+import Image from 'next/image'
 import { PulseLoader } from 'react-spinners'
 
 function Home() {
 
-  const [result, setResult] = useState('')
+  const [chatResult, setChatResult] = useState('')
+  const [imageResult, setImageResult] = useState('')
   const [textData, setTextData] = useState('')
   const [loading, setLoading] = useState(false)
   const formRef = useRef(null)
@@ -14,10 +16,12 @@ function Home() {
   const handleChat = async (e) =>{
     e.preventDefault()
     setLoading(true)
-    setResult('')
+    setChatResult('')
+    setImageResult('')
     setTextData('')
-    const response = await axios.post('https://react-laravel-open-ai-backend.herokuapp.com/api/example', textData)
-    setResult(response.data)
+    const response = await axios.post('https://react-laravel-open-ai-backend.herokuapp.com/api/example', textData) 
+    setChatResult(response.data[0])
+    setImageResult(response.data[1])
     setLoading(false)
   }
 
@@ -30,14 +34,19 @@ function Home() {
 
   return(
     <div className = {styles.page}>
-      <h1 className={styles.header}>Welcome to Soaren Management! Feel free to ask any questions</h1>
+      <h1 className={styles.header}>
+        Welcome to your personal organic chemistry tutor! Please feel free to ask anything! If you would like the structure of a compound, just type its name. 
+      </h1>
       <form className = {styles.form} ref = {formRef} onSubmit = {handleChat}>
         <input className = {styles.form__inputBar} type = 'text' value = {textData} onChange = {handleChange} />
       </form>
       <div className = {styles.loadingContainer}>
         {loading? <PulseLoader size={45} color="#36d7b7"/>: null}
       </div>
-      {result? <p className = {styles.response}>{result}</p>: null}
+      {chatResult? <p className = {styles.chatResponse}>{chatResult}</p>: null}
+      <div className = {styles.imageResponseContainer}>
+        {imageResult? <Image className= {styles.imageResponseContainer__imageResponse} width = {1000} height = {1000} src={imageResult} alt="Picture of the request"/>: null}
+      </div>
     </div>
   )
 }
